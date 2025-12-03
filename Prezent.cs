69a4563a4a6811cfc36f9a541d871c13b2352857
livvx540 <6fzgsn9kkp@privaterelay.appleid.com>
2025-12-03 13:10:10 +0100

    Add files via upload

---
+++ Prezent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramZaliczeniowy1
+{
+    class Prezent
+    {
+        public string Imie { get; set; }
+        public string Nazwa { get; set; }
+        public decimal Cena { get; set; }
+
+        public Prezent(string imie, string nazwa, decimal cena)
+        {
+            Imie = imie;
+            Nazwa = nazwa;
+            Cena = cena;
+        }
+        public override string ToString()
+        {
+            return $"{Imie} - {Nazwa} - {Cena} zł";
+        }
+    }
+}
